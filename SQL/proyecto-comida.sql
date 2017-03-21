979a1d0cc501acfcab675ece45ac69830dada22e
Pablo González <Pablo-G@users.noreply.github.com>
2017-03-21 13:43:09 -0600

    Corrige SQL

--- SQL/proyecto-comida.sql
+++ SQL/proyecto-comida.sql
@@ -1,27 +1,27 @@
 use gucumatz;
 
-CREATE TABLE TipoComida {
+CREATE TABLE TipoComida (
 	id Serial PRIMARY KEY,
 	nombre varChar(100) NOT NULL,
 	productos varChar(200) NOT NULL
-};
+);
 
-CREATE TABLE Puesto {
+CREATE TABLE Puesto (
 	id Serial PRIMARY KEY,
 	nombre varChar(100) NOT NULL,
 	referencias varChar(100) NOT NULL,
 	longitud Float NOT NULL,
-	latitud Float NOT NULL,
-};
+	latitud Float NOT NULL
+);
 
-CREATE TABLE FotosPuesto {
+CREATE TABLE FotosPuesto (
 	idPuesto Integer REFERENCES Puesto(id),
 	url varChar(100) NOT NULL,
 	PRIMARY KEY (idPuesto, url)
-}
+);
 
-CREATE TABLE PuestoTipoComida {
+CREATE TABLE PuestoTipoComida (
 	idPuesto Integer NOT NULL REFERENCES Puesto(id),
 	idComida Integer NOT NULL REFERENCES TipoComida(id),
 	PRIMARY KEY (idPuesto, idComida)
-};
+);
