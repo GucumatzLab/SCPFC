ba39fb2bc6f3882e16f6627bac37afe11a9caacd
Jorge Erick Rivera López <Jorge Erick Rivera Lpez>
2017-03-20 19:14:34 -0600

    Modifica archivo SQL

--- SQL/proyecto-comida.sql
+++ SQL/proyecto-comida.sql
@@ -1,6 +1,4 @@
 use gucumatz;
-
-<<<<<<< HEAD
 create table TipoComida(
 	id serial primary key,
 	nombre varchar(100) not null,
@@ -14,28 +12,4 @@ create table Puesto (
  	 longitud float not null,
  	 rutaImagen varchar(200)
 );
-=======
-create table Usuario (
-  id serial primary key,
-  nombre varchar(100) not null
-);
 
-create table Puesto (
-  id serial primary key
-);
-
-create table Comentario (
-  id serial primary key,
-  comentario varchar(1024) not null,
-  fecha date not null,
-  puesto_id integer not null references Puesto(id),
-  usuario_id integer not null references Usuario(id)
-);
-
-create table Calificacion (
-  id serial primary key,
-  calificacion float not null,
-  puesto_id integer not null references Puesto(id),
-  usuario_id integer not null references Usuario(id)
-);
->>>>>>> master
