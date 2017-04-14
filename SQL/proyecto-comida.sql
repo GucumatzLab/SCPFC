8083ae27692c0302f9fe3c08e8a6e10d058d8627
Luis Chacón <luis.chacon@ciencias.unam.mx>
2017-04-14 09:21:45 -0500

    Modifica el script de creación de la BD.
    Corrige la creación de llaves foráneas y agrega instrucciones para
    eliminar versiones anteriores de las tablas antes de crearlas.

--- SQL/proyecto-comida.sql
+++ SQL/proyecto-comida.sql
@@ -1,5 +1,11 @@
 use gucumatz;
 
+drop table if exists Comentario;
+drop table if exists Calificacion;
+drop table if exists FotosPuesto;
+drop table if exists Usuario;
+drop table if exists Puesto;
+
 create table Usuario (
   id serial primary key,
   nombre varchar(100) not null unique,
@@ -24,19 +30,24 @@ create table Comentario (
   id serial primary key,
   comentario varchar(1024) not null,
   fecha date not null,
-  puesto_id integer not null references Puesto(id),
-  usuario_id integer not null references Usuario(id)
+  puesto_id bigint unsigned not null,
+  usuario_id bigint unsigned not null,
+  foreign key (puesto_id) references Puesto(id),
+  foreign key (usuario_id) references Usuario(id)
 );
 
 create table Calificacion (
   id serial primary key,
   calificacion float not null,
-  puesto_id integer not null references Puesto(id),
-  usuario_id integer not null references Usuario(id)
+  puesto_id bigint unsigned not null,
+  usuario_id bigint unsigned not null,
+  foreign key (puesto_id) references Puesto(id),
+  foreign key (usuario_id) references Usuario(id)
 );
 
 CREATE TABLE FotosPuesto (
-	idPuesto Integer REFERENCES Puesto(id),
-	url varChar(100) NOT NULL,
-	PRIMARY KEY (idPuesto, url)
+  idPuesto bigint unsigned,
+  url varChar(100) NOT NULL,
+  PRIMARY KEY (idPuesto, url),
+  foreign key (idPuesto) references Puesto(id)
 );
