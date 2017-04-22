1a84a194bd639e0c6a3f90a2254828bc54732dc3
Jorge Erick Rivera López <Jorge Erick Rivera Lpez>
2017-04-21 22:33:25 -0500

    Modifica los casos de uso Agregar/Eliminar Puesto.
    - Mejora la estética de las interfaces.
    
    - Hace que el botón de eliminar puesto funcione.
    
    - Hace que las fotos de puestos se guarden en el sistema de archivos,
      aunque no en la base de datos.

--- scpfc/src/main/java/gucumatz/scpfc/controlador/EliminadorPuesto.java
+++ scpfc/src/main/java/gucumatz/scpfc/controlador/EliminadorPuesto.java
@@ -6,10 +6,13 @@ import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 import javax.faces.context.FacesContext;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import javax.annotation.PostConstruct;
 
 /**
  * Clase para controlar un objeto de la Clase Puesto
@@ -19,63 +22,98 @@ import java.util.Locale;
  */
 @ManagedBean
 @ViewScoped
-
 public class EliminadorPuesto implements Serializable {
 
     private PuestoJpaController jpaPuesto;
     private ComentarioJpaController jpaComentario;
     private CalificacionJpaController jpaCalificacion;
     private FotospuestoJpaController jpaFoto;
-
     private List<Puesto> puestos;
-    private String id = "";
+    private List<SelectItem> puestos2;
+    private String seleccionado;
 
-    public EliminadorPuesto() {
+    /**
+     * Metodo para inicializar los componentes de la clase.
+     */
+    @PostConstruct
+    public void init() {
         FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("es-Mx"));
         jpaPuesto = new FabricaControladorJpa().obtenerControladorJpaPuesto();
         jpaComentario = new FabricaControladorJpa().obtenerControladorJpaComentario();
         jpaCalificacion = new FabricaControladorJpa().obtenerControladorJpaCalificacion();
         jpaFoto = new FabricaControladorJpa().obtenerControladorJpaFotospuesto();
         puestos = new LinkedList<Puesto>(jpaPuesto.findPuestoEntities());
-    }
-
-    public List<Puesto> getPuestos() {
-        return this.puestos;
-    }
-
-    public void setPuestos(List<Puesto> l) {
-        this.puestos = l;
-    }
-
-    public String getId() {
-        return this.id;
+        seleccionado = "";
+        puestos2 = new LinkedList<SelectItem>();
+        SelectItemGroup g = new SelectItemGroup();
+        SelectItem[] si = new SelectItem[puestos.size()];
+        int i = 0;
+        for (Puesto p : puestos) {
+            si[i] = new SelectItem(p.getNombre(), p.getNombre());
+            i++;
+        }
+        g.setSelectItems(si);
+        puestos2.add(g);
     }
 
     /**
-     * Metodo para cambiar el valor de Puesto
+     * Metodo para obtener el nombre del puesto seleccionado.
      *
-     * @param nuevo con el que se reemplazará
+     * @return Nombre del puesto seleccionado.
      */
-    public void setId(String nuevo) {
-        this.id = nuevo;
+    public String getSeleccionado() {
+        return this.seleccionado;
     }
 
     /**
-     * Metodo con el que se hace la peticion al controlador para agregar un
+     * Metodo para cambiar el nombre del puesto seleccionado.
+     *
+     * @param nuevo - nuevo nombre.
+     */
+    public void setSeleccionado(String nuevo) {
+        this.seleccionado = nuevo;
+    }
+
+    /**
+     * Metodo para obtener la lista de SelectItem.
+     *
+     * @return Devuelve la Lista de SelectItem.
+     */
+    public List<SelectItem> getPuestos2() {
+        return this.puestos2;
+    }
+
+    /**
+     * Metodo para reemplazar la lista de SelectItem.
+     *
+     * @param l - Lista nueva.
+     */
+    public void setPuestos2(List<SelectItem> l) {
+        this.puestos2 = l;
+    }
+
+    /**
+     * Metodo con el que se hace la peticion al controlador para eliminar un
      * Puesto a la base de datos
      */
     public void elimina() {
 
         try {
-            if (jpaPuesto.findPuesto(Long.parseLong(this.id)) == null) {
-                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Advertencia:\nEl id no esta en la base de datos",null);
+            //if (jpaPuesto.findPuesto(Long.parseLong(this.id)) == null) {
+            if (this.seleccionado.equals("")) {
+                return;
+            }
+            Puesto p = jpaPuesto.findByNombre(this.seleccionado);
+            if (p == null) {
+                FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Advertencia:\nEl id no esta en la base de datos", null);
                 FacesContext.getCurrentInstance().addMessage(null, facesMessage);
                 return;
             }
             LinkedList<Comentario> com = new LinkedList<Comentario>(jpaComentario.findComentarioEntities());
             LinkedList<Calificacion> cal = new LinkedList<Calificacion>(jpaCalificacion.findCalificacionEntities());
             LinkedList<Fotospuesto> ft = new LinkedList<Fotospuesto>(jpaFoto.findFotospuestoEntities());
-            Puesto p = jpaPuesto.findPuesto(Long.parseLong(this.id));
+            //Puesto p = jpaPuesto.findPuesto(Long.parseLong(this.id));
+
             for (Comentario c : com) {
                 if (c.getPuestoId() == p) {
                     jpaComentario.destroy(c.getId());
@@ -93,21 +131,21 @@ public class EliminadorPuesto implements Serializable {
                     jpaFoto.destroy(f.getFotospuestoPK());
                 }
             }
-            jpaPuesto.destroy(Long.parseLong(this.id));
+            jpaPuesto.destroy(p.getId());
 
             FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Puesto Eliminado con exito", null);
             FacesContext.getCurrentInstance().addMessage(null, facesMessage);
             FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
             redirecciona();
-        } catch (NumberFormatException e) {
-            FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR\nDebe ser un numero", null);
-            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
         } catch (Exception e) {
-            FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR GRAVE\n"+e.getMessage(),null);
+            FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR GRAVE", e.getMessage());
             FacesContext.getCurrentInstance().addMessage(null, facesMessage);
         }
     }
 
+    /**
+     * Metodo para redireccionar la pagina actual a Administrar.xhtml.
+     */
     public void redirecciona() {
         try {
             FacesContext.getCurrentInstance().getExternalContext().redirect("./Administrar.xhtml");
